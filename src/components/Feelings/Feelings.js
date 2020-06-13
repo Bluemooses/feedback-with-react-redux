382f4c9ce51a546c0a28daff8af4d0eca965acd6
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-06-12 19:34:01 -0500

    Moving features

---
+++ src/components/Feelings/Feelings.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import axios from 'axios';
+
+class Feelings extends Component{
+
+}
+
+export default connect()(Feelings);
