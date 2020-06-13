28edfe8527086298c25d7eac1ae6034636d712ad
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-06-12 20:19:10 -0500

    Form field select 1-4 works properly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import Header from '../Header/Header';
+import Feelings from '../Feelings/Feelings';
 
 
 class App extends Component {
@@ -12,7 +13,7 @@ class App extends Component {
           <Router>
             <header className="App-header">
                <Header />
-                               
+               <Feelings />                               
             </header>
           </Router>    
         <br/>
--- src/components/Feelings/Feelings.js
+++ src/components/Feelings/Feelings.js
@@ -9,10 +9,22 @@ class Feelings extends Component{
         ready: false
     }
 
-    componentDidMount = () => {
-        this.props.dispatch({
+    constructor() {
+        super();
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-        })
+    componentDidMount = () => {
+        // this.props.dispatch({
+
+        // })
+    }
+
+    handleChange(event){
+        this.setState({
+            rating: event.target.value
+        });
+        console.log("rating:", event.target.value)
     }
 
 
@@ -23,19 +35,19 @@ class Feelings extends Component{
                     <form>
                         <h3 className="question">How are you feeling today?</h3>
                             <label>
-                                <input type="radio" value="1" checked={false} />
+                                <input type="radio" value="1" checked={this.state.rating === "1"} onChange={this.handleChange} />
                                 1
                             </label>
                             <label>
-                                <input type="radio" value="2" checked={false} />
+                                <input type="radio" value="2" checked={this.state.rating === "2"} onChange={this.handleChange}/>
                                 2
                             </label>                        
                             <label>
-                                <input type="radio" value="3" checked={false} />
+                                <input type="radio" value="3" checked={this.state.rating === "3"} onChange={this.handleChange} />
                                 3
                             </label>                        
                             <label>
-                                <input type="radio" value="4" checked={false} />
+                                <input type="radio" value="4" checked={this.state.rating === "4"} onChange={this.handleChange}/>
                                 4
                             </label>
                             <button className="nextBtn" disabled={!this.state.ready} onClick={this.nextBtnHandle}></button>
