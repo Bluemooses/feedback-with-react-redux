e03e8c3486ab2c817da2b7fb5f22a811ab0b7d5c
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-06-12 20:00:10 -0500

    Added input radios for feedback entry

--- src/components/Feelings/Feelings.js
+++ src/components/Feelings/Feelings.js
@@ -4,6 +4,49 @@ import axios from 'axios';
 
 class Feelings extends Component{
 
+    state={
+        rating: 0,
+        ready: false
+    }
+
+    componentDidMount = () => {
+        this.props.dispatch({
+
+        })
+    }
+
+
+    render(){
+        return(
+            <div>
+                <section className="surveySays">
+                    <form>
+                        <h3 className="question">How are you feeling today?</h3>
+                        <label>
+                            <input type="radio" value="1" checked={false} />
+                            1
+                        </label>
+                        <label>
+                            <input type="radio" value="2" checked={false} />
+                            2
+                        </label>                        
+                        <label>
+                            <input type="radio" value="3" checked={false} />
+                            3
+                        </label>
+                        
+                        <label>
+                            <input type="radio" value="4" checked={false} />
+                            4
+                        </label>
+                    </form>
+                    
+                    
+
+                </section>
+            </div>
+        )
+    }
 }
 
 export default connect()(Feelings);
